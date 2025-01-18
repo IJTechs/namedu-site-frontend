d05c607015feca7dfa9488a3faacae2674b5fd97
Isokjon-Osimjonov <osimjonovisoqjon2004@gmail.com>
2025-01-18 10:17:19 +0900

    chore: Initial commit with project setup v1.0.0

---
+++ README.md
@@ -0,0 +1,86 @@
+# **Namedu.uz**
+
+### https://namedu.uz
+
+---
+
+![React](https://img.shields.io/badge/React-18.0+-blue.svg) ![TypeScript](https://img.shields.io/badge/TypeScript-5.5+-blue.svg) ![TailwindCSS](https://img.shields.io/badge/TailwindCSS-3.4+-teal.svg) ![Vite](https://img.shields.io/badge/Vite-5.4+-purple.svg)
+
+---
+
+## 📜 **Installation & Setup**
+
+Follow these steps to set up the **Namedu.uz** on your local machine:
+
+---
+
+### **1. Prerequisites**
+
+Make sure the following tools are installed on your system:
+
+- **Node.js**:(v20)
+  - Install via [Node.js Official Website](https://nodejs.org/) or use `nvm` for managing multiple versions.
+- **Git**: Latest version.
+  - Download from [Git Official Website](https://git-scm.com/).
+- **Yarn**: Latest version.
+  - Install globally using npm:
+    ```bash
+    npm install -g yarn
+    ```
+
+---
+
+### **2. Clone the Repository**
+
+Clone the repository from GitHub and navigate into the project directory:
+
+```bash
+git clone https://github.com/IJTechs/namedu-site-frontend.git
+cd namedu-site-frontend
+```
+
+---
+
+### **3. Install Dependencies**
+
+Install all required project dependencies using Yarn:
+
+```bash
+yarn install
+```
+
+---
+
+### **4. Configure Environment Variables**
+
+Set up the environment variables required for the project:
+
+1. Create a `.env` file in the root directory:
+   ```bash
+   touch .env
+   ```
+2. Add the variables (use `.env.example` as a reference):
+
+   ```env
+
+   ```
+
+---
+
+### **5. Run Locally**
+
+Start the development server:
+
+```bash
+yarn dev
+```
+
+Visit the application at [http://localhost:3000](http://localhost:3000).
+
+---
+
+## 📝 **Contact**
+
+- **Email**: [osimjonovisoqjon2004@gmail.com](mailto:osimjonovisoqjon2004@gmail.com)
+- **GitHub**: [Isokjon-Osimjonov](https://github.com/Isokjon-Osimjonov)
+- **Telegram**: [Isokjon](https://t.me/isokjon_io)
---
+++ vite.config.ts
@@ -0,0 +1,94 @@
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import dynamicImport from 'vite-plugin-dynamic-import';
+
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    // Plugins
+    plugins: [
+      react(),
+
+      isProduction &&
+        visualizer({
+          filename: './dist/stats.html',
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+        }),
+
+      viteCompression({ algorithm: 'brotliCompress', ext: '.br' }),
+      viteCompression({ algorithm: 'gzip', ext: '.gz' }),
+      dynamicImport(),
+    ],
+
+    // Path alias
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@api': path.resolve(__dirname, './src/api'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@constants': path.resolve(__dirname, './src/constants'),
+        '@helpers': path.resolve(__dirname, './src/helpers'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+        '@layouts': path.resolve(__dirname, './src/layouts'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@queries': path.resolve(__dirname, './src/queries'),
+        '@requests': path.resolve(__dirname, './src/requests'),
+        '@routes': path.resolve(__dirname, './src/routes'),
+        '@services': path.resolve(__dirname, './src/services'),
+        '@styles': path.resolve(__dirname, './src/styles'),
+        '@types': path.resolve(__dirname, './src/types'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+      },
+    },
+
+    // Server settings
+    server: {
+      port: 3000,
+      open: true,
+    },
+
+    // Build settings
+    build: {
+      target: 'esnext',
+
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+          pure_funcs: ['console.info', 'console.debug'],
+        },
+        mangle: {
+          toplevel: true,
+        },
+        format: {
+          comments: false,
+        },
+      },
+
+      sourcemap: false,
+
+      cssCodeSplit: true,
+
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+      },
+    },
+
+    optimizeDeps: {
+      include: ['react', 'react-dom'],
+    },
+  };
+});
