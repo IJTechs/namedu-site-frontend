45b0ebb33a7d002c15b298c9edcbc673479e8a9f
Isokjon-Osimjonov <osimjonovisoqjon2004@gmail.com>
2025-03-15 21:51:50 +0900

    fix: api url

--- src/utils/constants/api-urls.ts
+++ src/utils/constants/api-urls.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://api.namedu.uz/api/v1';
-export const BASE_URL = 'http://localhost:8000/api/v1';
+export const BASE_URL = 'https://api.namedu.uz/api/v1';
+// export const BASE_URL = 'http://localhost:8000/api/v1';
 
 export const API_URLS = {
   NEWS: {
